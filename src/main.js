0d48e98351d35c47d81e3d01cfa6fae5cbd4ee1d
liuleiCain <ll520cj1314>
2021-11-05 15:17:27 +0800

    更改

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import createRouter from './router'
-import router from './router'
-
-const app = createApp(App).use(router)
-app.use(createRouter)
-app.mount('#app')
